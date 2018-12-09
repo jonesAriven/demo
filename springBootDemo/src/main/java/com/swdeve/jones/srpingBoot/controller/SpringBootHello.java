357354575ba1bda9da48892a778942ef5dc8fccf
jonesAriven <1750163946@qq.com>
2018-12-09 18:52:35 +0800

    20181209-对pom文件添加部分说明

---
+++ springBootDemo/src/main/java/com/swdeve/jones/srpingBoot/controller/SpringBootHello.java
@@ -0,0 +1,22 @@
+package com.swdeve.jones.srpingBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+//@ResponseBody//该注解若写在类上表示该类下的所有方法都是直接将方法的返回值写到浏览器端
+@RestController // 可以用@RestController注解代替@Controller和@ResponseBody两个注解
+public class SpringBootHello {
+
+	@RequestMapping("/helloOne")
+	public String helloOne() {
+		return "helloOne";
+	}
+
+	@RequestMapping("/helloTwo")
+	public String helloThree() {
+		return "helloTwo";
+	}
+}
--- springBootDemo/src/main/java/com/swdeve/jones/srpingBoot/controller/SpringBootHelloWorld.java
+++ springBootDemo/src/main/java/com/swdeve/jones/srpingBoot/controller/SpringBootHelloWorld.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SpringBootHelloWorld {
 	
-	@ResponseBody
-	@RequestMapping("/hello")
-	public String hello() {
+	@ResponseBody//表示直接将方法的返回值写到浏览器端
+	@RequestMapping("/helloWorld")
+	public String helloWorld() {
 		return "helloWorld";
 	}
 }
