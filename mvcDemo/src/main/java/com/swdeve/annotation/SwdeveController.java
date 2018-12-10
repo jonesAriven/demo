92cc8e7fdb1584522fb5ff5ed6fc3c0da379bf46
jonesAriven <1750163946@qq.com>
2018-12-10 21:41:20 +0800

    20181210-修改字符集

--- mvcDemo/src/main/java/com/swdeve/annotation/SwdeveController.java
+++ mvcDemo/src/main/java/com/swdeve/annotation/SwdeveController.java
@@ -6,9 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)  //±íÊ¾×¢½âÊ¹ÓÃÔÚÔÚÀàÉÏ
-@Retention(RetentionPolicy.RUNTIME) //±íÊ¾×¢½âÔÚÓ¦ÓÃÆô¶¯Ê±¼ÓÔØ
-@Documented //°üº¬ÔÚJAVA docÖĞ
+@Target(ElementType.TYPE)  //è¡¨ç¤ºæ³¨è§£ä½¿ç”¨åœ¨åœ¨ç±»ä¸Š
+@Retention(RetentionPolicy.RUNTIME) //è¡¨ç¤ºæ³¨è§£åœ¨åº”ç”¨å¯åŠ¨æ—¶åŠ è½½
+@Documented //åŒ…å«åœ¨JAVA docä¸­
 public @interface SwdeveController {
 	String value () default "";
 }
