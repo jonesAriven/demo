9cb2bd49dd7cfbc038c04636600c20a1a00a5ec2
jonesAriven <1750163946@qq.com>
2018-12-22 22:13:13 +0800

    20181222-添加日志框架

--- spring-boot-web-restfulcrud/spring-boot-web-restfulcrud/src/main/java/com/swdeve/springboot/controller/EmpController.java
+++ spring-boot-web-restfulcrud/spring-boot-web-restfulcrud/src/main/java/com/swdeve/springboot/controller/EmpController.java
@@ -1,5 +1,7 @@
 package com.swdeve.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +11,21 @@ import com.swdeve.springboot.dao.EmployeeDao;
 
 @Controller
 public class EmpController {
-
-	@Autowired//有@Autowired 注解就不需要get，set方法了
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired // 有@Autowired 注解就不需要get，set方法了
 	EmployeeDao employeeDao;
-	
+
 	@GetMapping("/emps")
 	public String list(Model model) {
+		// 日志的级别；
+		// 由低到高 trace<debug<info<warn<error
+		// 可以调整输出的日志级别；日志就只会在这个级别以以后的高级别生效
+		logger.trace("这是trace日志...");
+		logger.debug("这是debug日志...");
+		// SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
+		logger.info("这是info日志...");
+		logger.warn("这是warn日志...");
+		logger.error("这是error日志...");
 		model.addAttribute("emps", employeeDao.getAll());
 		return "emp/list";
 	}
